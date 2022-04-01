9b6f495b7700a1fb760f574f543f5b3877003a58
nyomanjyotisa <jyotisa1616@gmail.com>
2022-04-01 15:42:52 +0800

    bean primary

--- src/main/java/id/jyotisa/learnspring/DuplicateConfiguration.java
+++ src/main/java/id/jyotisa/learnspring/DuplicateConfiguration.java
@@ -3,10 +3,12 @@ package id.jyotisa.learnspring;
 import id.jyotisa.learnspring.data.Foo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DuplicateConfiguration {
 
+    @Primary
     @Bean
     public Foo foo1(){
         return new Foo();
--- src/test/java/id/jyotisa/learnspring/DuplicateTest.java
+++ src/test/java/id/jyotisa/learnspring/DuplicateTest.java
@@ -22,10 +22,13 @@ public class DuplicateTest {
     void getBean() {
         ApplicationContext context =new AnnotationConfigApplicationContext(DuplicateConfiguration.class);
 
+        Foo foo = context.getBean(Foo.class);
         Foo foo1 = context.getBean("foo1", Foo.class);
         Foo foo2 = context.getBean("foo2", Foo.class);
 
+        Assertions.assertSame(foo, foo1);
         Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo, foo2);
 
     }
 }
